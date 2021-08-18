9920c0492cbd9669be3887f763cae746db6367a1
David Barral <david@trabe.io>
2021-08-18 13:16:49 +0200

    [FIX] repo owner was hardcoded

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const { octokit } = require("./github.js");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const getPullRequest = async (password, repo, pull_number) => {
   const github = octokit(password);
-  
+
   const pr = await github.getPR(repo, pull_number);
 
   return pr;
-}
+};
 
 (async () => {
   try {
     const status = core.getInput("job-status");
-    let state = status === "success" ? "SUCCESS" : status === "failure" ? "FAILURE" : "CANCELLED";
-    let color = status === "success" ? "#2e993e" : status === "failure" ? "#bd0f26" : "#d29d0c";
-    
+    let state =
+      status === "success"
+        ? "SUCCESS"
+        : status === "failure"
+        ? "FAILURE"
+        : "CANCELLED";
+    let color =
+      status === "success"
+        ? "#2e993e"
+        : status === "failure"
+        ? "#bd0f26"
+        : "#d29d0c";
+
     let branch = github.context.ref.split("/").splice(-1)[0];
-    let runDetails = `Run details: <https://github.com/trabe/${github.context.repo.repo}/actions/runs/${github.context.runId} | ${github.context.runId} run details> \n`;
-    let workflow = github.context.workflow.split("/").splice(-1)[0].replace(".yml", "");
+    let runDetails = `Run details: <https://github.com/${github.context.repo.repository_owner}/${github.context.repo.repo}/actions/runs/${github.context.runId} | ${github.context.runId} run details> \n`;
+    let workflow = github.context.workflow
+      .split("/")
+      .splice(-1)[0]
+      .replace(".yml", "");
     let eventInfo = core.getInput("event");
     const jobName = github.context.job;
 
@@ -28,22 +41,33 @@ const getPullRequest = async (password, repo, pull_number) => {
       case "push":
         if (github.context.ref.includes("tags")) {
           content = `New tag: *${github.context.ref.split("/").splice(-1)[0]}*`;
-        }
-        else {
-          content = `Push on *${branch}*. \n Commit: ${JSON.parse(eventInfo).head_commit.url}`; 
+        } else {
+          content = `Push on *${branch}*. \n Commit: ${
+            JSON.parse(eventInfo).head_commit.url
+          }`;
         }
         break;
       case "pull_request":
-        let pr = await getPullRequest(core.getInput("repo-token"),github.context.repo.repo, github.context.payload.pull_request.number);
-        content = `Pull Request #${github.context.payload.pull_request.number}: ${pr.title} \n *${core.getInput("head_ref")}* -> *${core.getInput("base_ref")}* \n ${github.context.payload.pull_request.html_url}`;
+        let pr = await getPullRequest(
+          core.getInput("repo-token"),
+          github.context.repo.repo,
+          github.context.payload.pull_request.number
+        );
+        content = `Pull Request #${
+          github.context.payload.pull_request.number
+        }: ${pr.title} \n *${core.getInput("head_ref")}* -> *${core.getInput(
+          "base_ref"
+        )}* \n ${github.context.payload.pull_request.html_url}`;
         break;
       case "workflow_dispatch":
         content = `Manual trigger`;
         break;
       default:
-        content = `On *${branch}* branch \n Commit: ${JSON.parse(eventInfo).head_commit.url}`;
-    };
-    
+        content = `On *${branch}* branch \n Commit: ${
+          JSON.parse(eventInfo).head_commit.url
+        }`;
+    }
+
     const postData = JSON.stringify({
       channel: `${core.getInput("channel")}`,
       attachments: [
@@ -54,15 +78,15 @@ const getPullRequest = async (password, repo, pull_number) => {
               type: "section",
               text: {
                 type: "mrkdwn",
-                text: `Github Action *${workflow}*:*${jobName}* *${state}*`
-              }
-            },    
+                text: `Github Action *${workflow}*:*${jobName}* *${state}*`,
+              },
+            },
             {
               type: "section",
               text: {
                 type: "mrkdwn",
-                text: `${runDetails}${content}`
-              }
+                text: `${runDetails}${content}`,
+              },
             },
             {
               type: "context",
@@ -70,28 +94,29 @@ const getPullRequest = async (password, repo, pull_number) => {
                 {
                   type: "image",
                   image_url: `https://github.com/${github.context.actor}.png`,
-                  alt_text: "images"
+                  alt_text: "images",
                 },
                 {
                   type: "mrkdwn",
-                  text: `*Author:* ${github.context.actor}`
-                }
-              ]
+                  text: `*Author:* ${github.context.actor}`,
+                },
+              ],
             },
             {
               type: "context",
               elements: [
                 {
                   type: "image",
-                  image_url: "https://avatars0.githubusercontent.com/u/9919?s=280&v=4",
-                  alt_text: "images"
+                  image_url:
+                    "https://avatars0.githubusercontent.com/u/9919?s=280&v=4",
+                  alt_text: "images",
                 },
                 {
                   type: "mrkdwn",
-                  text: `<https://github.com/trabe/${github.context.repo.repo}| trabe/${github.context.repo.repo}>`
-                }
-              ]
-            }
+                  text: `<https://github.com/${github.context.repo.repository_owner}/${github.context.repo.repo}| ${github.context.repo.repository_owner}/${github.context.repo.repo}>`,
+                },
+              ],
+            },
           ],
         },
       ],
@@ -106,11 +131,11 @@ const getPullRequest = async (password, repo, pull_number) => {
         Authorization: `Bearer ${core.getInput("slack-bot-token")}`,
         Accept: "application/json",
       },
-    })
+    });
     if (!res.ok) {
       throw new Error(`Server error ${res.status}`);
     }
   } catch (error) {
     core.setFailed(error.message);
   }
-})()
+})();
